538a723c9128e543264568f8655588b86aaf07f5
Bala <sudhagouthirubi@gmail.com>
2021-11-28 22:28:28 +0530

    * added clientside script for data sub

--- APIclient.js
+++ APIclient.js
@@ -1,13 +1,46 @@
 let axios=require("axios")
+let socket=null
+const io=require("socket.io-client")
+function TestRestApi(){
+    axios.get("http://localhost:8080/homepage")
+    .then(r=>{
+        console.log("Data recieved from home")
+        console.log(r.data.pulse)
+    },err=>console.log(err))
 
-axios.get("http://localhost:8080/homepage")
-.then(r=>{
-    console.log("Data recieved from home")
-    console.log(r.data.pulse)
-},err=>console.log(err))
+    axios.get("http://localhost:8080/serverAPI")
+    .then(r=>{
+        console.log(r.data.pulse)
+        console.log("data recieved ")
+    },err=>console.log(err))
+}
 
-axios.get("http://localhost:8080/serverAPI")
-.then(r=>{
-    console.log(r.data.pulse)
-    console.log("data recieved ")
-},err=>console.log(err))
+function TestSocketApi(){ 
+  socket=io("http://localhost:5000",{secure:true})
+//   socket=io("https://argo-server-1.herokuapp.com/",{secure:true})
+  socket.on('connect',function(){
+    console.log("Socket connected to server")
+  })
+  socket.on('ready',()=>{
+      socket.emit('typeUpdate',{type:"view"})
+  })
+  socket.on('disconnect',()=>console.log("Disconnected from server"))
+  socket.on('success',()=>console.log("type update done"))
+  socket.on('valueUpdate',({data})=>{
+    console.log("data recieved is");console.log(data);
+  })
+  socket.on('nosenders',()=>console.log("No senders ready"))
+  socket.on('error',()=>console.log("Error while trying to connect"))
+}
+
+function SubtoSocket(callBack=(data)=>{console.log(data)}){
+    if(socket){
+        socket.on('newValue',data=>{
+            console.log("data recieved is "+JSON.stringify(data))
+            callBack(data)
+        })
+    }
+}
+
+TestSocketApi()
+SubtoSocket()
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const socket = require("socket.io");
 var serverRouter=require('./SeperateRouter')
 app.use(cors())
 
-
+var dataSenders=[]
+var dataViewers=[]
 
 const PORT = process.env.PORT||5000;
 
@@ -14,19 +15,45 @@ app.get('/getinfo', function(req, res){
   res.send({port:PORT,auth:"None"});
 });
 
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
-
 // Static files
 app.use(express.static("public"));
 
 app.use("/restpath",serverRouter)
 
+
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
 // Socket setup
 const io = socket(server);
 
 io.on("connection", function (socket) {
-  console.log("Made socket connection");
+  console.log("Made socket connection: "+socket.id);
+  io.emit("ready",{})
+  socket.on("valueUpdate",data=>{console.log(data)})
+  socket.on("typeUpdate",data=>{
+      if(data.type=="send"){
+          dataSenders.push(socket)
+          console.log("Accepted a Sender")
+      }
+      else if(data.type=="view"){
+          dataViewers.push(socket)
+          console.log("Accepted a Viewer")
+          if(dataSenders.length){
+              dataSenders[0].on("valueUpdate",data=>{
+                  socket.emit("newValue",data)
+              })
+          }else{
+              console.log("in no senders")
+              socket.emit('nosenders',{senders:dataSenders})
+          }
+      }
+      socket.emit('success',{})
+  })
+  socket.on('disconnect',()=>{
+      console.log("socket disconnected "+socket.id )
+  })
 }); 
+
