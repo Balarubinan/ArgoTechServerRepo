c94c446bf89db28da15a86fc361ed0de3d5d5274
Bala <sudhagouthirubi@gmail.com>
2021-11-29 12:36:53 +0530

    * added public use static

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var dataViewers=[]
 const PORT = process.env.PORT||5000;
 
 app.use(express.static(path.join(__dirname, 'agro-react-app/build')));
+app.use(express.static('agro-react-app/public'));
 app.use(express.json()); // was originally body-parser.json()
 
 
