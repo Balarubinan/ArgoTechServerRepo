f328fd1e48beede0c22e98935828bdbd0befc744
Bala <sudhagouthirubi@gmail.com>
2021-11-29 12:20:09 +0530

    * logging path

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const server = app.listen(PORT, function () {
 });
 
 //  serving react
+console.log(`${process.cwd()}`)
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, 'agro-react-app/build/index.html'));
 });
