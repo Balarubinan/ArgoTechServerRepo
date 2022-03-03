bc221c1bb73783132345ac2d742a4e673372da6f
Bala Rubinan N <sudhagouthirubi@gmail.com>
2022-03-03 15:03:12 +0530

    Update URLS.js

--- reactwebagro/src/URLS.js
+++ reactwebagro/src/URLS.js
@@ -3,5 +3,5 @@ const devUrl="http://localhost:5000"
 const prodUrl="https://argo-server-1.herokuapp.com"
 // change to dev before dev and change to prod before push
 module.exports={
-    baseUrl:devUrl
+    baseUrl:prodUrl
 }
